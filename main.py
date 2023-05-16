3fb2ca93e28866017e360354aa5722d36dbcb986
duking119 <huoyanfeng7@gmail.com>
2023-05-16 13:48:38 +0800

    new files

---
+++ main.py
@@ -0,0 +1,12 @@
+import csv
+
+
+class Item:
+    def __init__(self,name:str,price:float,quantity=0):
+        # assert price and quantity's values are greater than or equal to zero
+        assert price>=0, f'Price {price} must be greater than or equal to zero!'
+        assert quantity>=0, f'Quantity {quantity} must be greater than or equal to zero!'
+
+        self.name = name
+        self.price = price
+        self.quantity = quantity
