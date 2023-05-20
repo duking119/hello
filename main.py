e8a654c97b551be2bab20e8b7c4ea54a71295d1b
duking119 <huoyanfeng7@gmail.com>
2023-05-20 14:13:34 +0800

    newchange

--- main.py
+++ main.py
@@ -4,31 +4,32 @@ import csv
 class Item:
     pay_rate = 0.8
     all = []
-    def __init__(self,name:str,price:float,quantity=0):
+
+    def __init__(self, name: str, price: float, quantity=0):
         # assert price and quantity's values are greater than or equal to zero
-        assert price>=0, f'Price {price} must be greater than or equal to zero!'
-        assert quantity>=0, f'Quantity {quantity} must be greater than or equal to zero!'
+        assert price >= 0, f'Price {price} must be greater than or equal to zero!'
+        assert quantity >= 0, f'Quantity {quantity} must be greater than or equal to zero!'
 
         self.name = name
         self.price = price
         self.quantity = quantity
-        
+
         Item.all.append(self)
-    
+
     # define a calculation of the price and quantity
     def calculate_total_price(self):
-        return self.prices * self.quantity
-        
-    #define a representation of items
+        return self.price * self.quantity
+
+    # define a representation of items
     def __repr__(self):
-        return f'Item ({self.name}, {self.price}, {self.quantity})'
-    
+        return f'{self.__class__.__name__} ({self.name}, {self.price}, {self.quantity})'
+
     @classmethod
     def instantiate_from_csv(cls):
-        with open ('items.csv', 'r') as f:
+        with open('items.csv', 'r') as f:
             reader = csv.DictReader(f)
             items = list(reader)
-            
+
         for item in items:
             Item(
                 name=item.get('name'),
@@ -40,7 +41,7 @@ class Item:
     def is_integer(number):
         if isinstance(number, float):
             return number.is_integer()
-        elif isinstance(number,int):
+        elif isinstance(number, int):
             return True
         else:
             return False
@@ -48,8 +49,20 @@ class Item:
     def apply_discount(self):
         self.price = self.price * self.pay_rate
 
-# item1 = Item('MyItem', 500, 5)
-# item1.apply_discount()
 
-# print(Item.all)
-print(Item.is_integer(7.3))
+class Phone(Item):
+    def __init__(self, name: str, price: float, quantity=0, broken_phones=0):
+        # Call to super function
+        super().__init__(name, price, quantity)
+        assert broken_phones >= 0, f'Broken phone {broken_phones} must be greater than or equal to zero!'
+
+        self.broken_phones = broken_phones
+
+
+# Item.instantiate_from_csv()
+phone1 = Phone("jsvPhone10", 500, 5, 1)
+phone2 = Phone("jsvPhone20", 700, 3, 1)
+
+print(phone1.calculate_total_price())
+print(Item.all)
+print(Phone.all)
